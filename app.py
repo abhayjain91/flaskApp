587926b7e09860d5b47d549083c9539a88282f50
abhayjain91 <abhayjain91@gmail.com>
2023-09-01 12:31:16 +0530

    Added auto deploy

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, Blueprint, jsonify
 
 app = Flask(__name__)
+app_blueprint = Blueprint('default', __name__, template_folder='templates', static_folder='static')
 
 
 @app.route('/')
@@ -8,6 +10,7 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World!'
+@app_blueprint.route('/hello')
+def default_route():
+    """Confirm that the application is working."""
+    return jsonify({'hello': 'from template api auto-deployed!'}), 200
