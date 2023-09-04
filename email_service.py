572808b475c2a361b64992e8cf7c69dee9dcd776
abhayjain91 <abhayjain91@gmail.com>
2023-09-04 18:32:14 +0530

    Add sendgrid email service

--- email_service.py
+++ email_service.py
@@ -23,3 +23,28 @@ toaddrs = '----'
 server.sendmail(fromaddr, toaddrs, message.as_string())
 
 server.quit()
+
+
+def using_sendgrid():
+    import sendgrid
+    import os
+    from sendgrid.helpers.mail import Mail, Email, To, Content
+
+    my_sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+
+    # Change to your verified sender
+    from_email = Email("your_email@example.com")
+
+    # Change to your recipient
+    to_email = To("destination@example.com")
+
+    subject = "Lorem ipsum dolor sit amet"
+    content = Content("text/plain", "consectetur adipiscing elit")
+
+    mail = Mail(from_email, to_email, subject, content)
+
+    # Get a JSON-ready representation of the Mail object
+    mail_json = mail.get()
+
+    # Send an HTTP POST request to /mail/send
+    response = my_sg.client.mail.send.post(request_body=mail_json)
